10c7f80da1e0ae0463d142c06b912310fc2dc15d
creativealex1 <rudnik.2002@inbox.ru>
2024-06-21 13:24:10 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+a = int(input('Введите число: '))
+b = int(input('Введите число: '))
+c = int(input('Введите число: '))
+if a == b == c :
+    print(3)
+elif a == b or b == c or a == c :
+    print (2)
+elif a != b or b != c or a != c :
+    print(0)
